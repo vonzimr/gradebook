f5073f7ba49dc0f1478ee07f268528150992b338
vonzimr <vonzimr@gmail.com>
2018-02-19 14:47:55 -0800

    stash

---
+++ gradebook-client/src/components/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="login">
+        Hello?? {{ this.$route.params.id}}
+    </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+      name: 'login'
+  };
+</script>
+
+<style>
+</style>
